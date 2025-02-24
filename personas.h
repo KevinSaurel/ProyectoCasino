e8981ebd1dec786159b3089c437d658e23dd5814
Mikel Oyarzabal Riaño <145966883+MikelOyarzabal@users.noreply.github.com>
2025-02-24 19:57:26 +0100

    personas.h prueba

---
+++ personas.h
@@ -0,0 +1,11 @@
+typedef struct 
+{   
+    char nombre[50];
+    char apellido[50];
+    int edad;
+    char sexo;
+    char direccion[50];
+    char telefono[50];
+    char email[50];
+
+}Persona;
