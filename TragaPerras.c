dbbca63d4716129eed1f5af14b0158da35951684
KevinSaurel <kevin.saurel@opendeusto.es>
2025-02-24 18:44:37 +0100

    PruebaTragaperras

--- TragaPerras.c
+++ TragaPerras.c
@@ -1,6 +1,40 @@
 #include <stdio.h>
+
 // A TRAGGAAAAARRRRRRRRRRRRRR 
 void TragaPerras(){
-     printf("");
+     int flag = 0;
+     
+           printf("Tragaperras\n");
+               printf("1. Jugar\n");
+               printf("2. Salir\n");
+               int opcion;
+               scanf("%d", &opcion);
+               switch(opcion){
+                    case 1:
+                         printf("Jugando\n");
+                         StartTragaPerras();
+                    case 2:
+                         flag = 1;
+                         
+                    default:
+                         printf("Opcion no valida\n");
+                         
+               }
+           
 
+     
+
+}
+void StartTragaPerras(){
+    int  flag=0;
+     while (flag==0)
+     {
+          sleep(1000);
+          printf("|| %i || %i || %i ||\n",rand()%10,rand()%10,rand()%10);
+          scanf("%d", &flag);
+          if(flag==1){
+               "acabo";
+          }
+     }
+     
 }
--- main.c
+++ main.c
@@ -1,8 +1,10 @@
 #include <stdio.h>
 
+
 int main(int argc, char const *argv[]){
 
 //Yahoooooooo viva el casino (Prueba 1 Commit con VSCODE)
+printf("Holla Mudo Proyecto Casino");
 
 return 0;
 }
