38e3db17502473129eee33862aa851289d707a22
KevinSaurel <kevin.saurel@opendeusto.es>
2025-02-24 20:27:37 +0100

    TragaPerrasPushBotton1

--- TragaPerras.c
+++ TragaPerras.c
@@ -2,6 +2,8 @@
 #include "juegos.h"
 #include <windows.h> // para el sleep
 #include <stdlib.h>// para el rands
+#include <time.h> // para el time
+#include <conio.h>   // For kbhit() //waits for a key to be pressed
 // A TRAGGAAAAARRRRRRRRRRRRRR 
 void TragaPerras(){
      int flag = 0;
@@ -15,11 +17,13 @@ void TragaPerras(){
                     case 1:
                          printf("Jugando\n");
                          StartTragaPerras();
+                         break;
                     case 2:
                          flag = 1;
-                         
+                         break;
                     default:
                          printf("Opcion no valida\n");
+                         break;
                          
                }
            
@@ -29,14 +33,18 @@ void TragaPerras(){
 }
 void StartTragaPerras(){
     int  flag=0;
+    srand(time(NULL));
      while (flag==0)
      {
           Sleep(1000);
           printf("|| %i || %i || %i ||\n",rand()%10,rand()%10,rand()%10);
-          scanf("%d", &flag);
-          if(flag==1){
-               "acabo";
-          }
+          //scanf("%i", &flag);
+          if (_kbhit()) {  
+               printf("Press 1 to stop: ");
+               scanf("%i", &flag);
+           }
+
+          
      }
 }
      
