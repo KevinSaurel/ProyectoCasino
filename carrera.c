8495217273c83cbf05dfb4fbbaf585cf9a377e78
Gorka Villalba <gorka.villalba@opendeusto.es>
2025-03-03 21:23:00 +0100

    He cambioda la implementacion de carrera para que soporte carreras de mas de 3 caballos

--- carrera.c
+++ carrera.c
@@ -9,6 +9,7 @@
 
 
 #define MAX_DISTANCE 50  // Distancia de la carrera
+#define MAX_CABALLOS 3 //numero de caballos en la carrera, modificar esto para que se pueda elegir la cantidad de caballos
 void carrera(Cliente c){//el menu se crea en el main, y las partes de arriba en la clase menu
      int flag = 0;
      
@@ -50,7 +51,7 @@ void carrera(Cliente c){//el menu se crea en el main, y las partes de arriba en 
 }
 void startCarrera(Cliente *c){
     int  flag=0;
-    int cantidadCaballos = 3;//modificar esto para que se pueda elegir la cantidad de caballos
+    //modificar esto para que se pueda elegir la cantidad de caballos
     int apuesta = 10;//habria que escribir un scan para pedir la apuesta, pero habria que poner comprobaciones de que no apueste en negativo
     //primero lo probare con una apuesta fija
     printf("\n                           CARRERA EN MARCHA...                               \n");
@@ -58,23 +59,38 @@ void startCarrera(Cliente *c){
     
     
 
-    int c1 = 0, c2 = 0, c3 = 0;
+    /*int c1 = 0, c2 = 0, c3 = 0;*/
+    Caballo listaCaballos[MAX_CABALLOS];
+    for (int i = 0; i < MAX_CABALLOS; i++)//inicializamos los caballos
+    {
+        listaCaballos[i].numero = i+1;
+        listaCaballos[i].posicion = 0;
+    }//de esta forma es facilmente escalable a mayor cantidad de caballos solo cambiando el numero maximo de caballos
 
     srand(time(NULL)); // Inicializar la semilla de números aleatorios
 
-    while (c1 < MAX_DISTANCE && c2 < MAX_DISTANCE && c3 < MAX_DISTANCE) {
-        c1 += rand() % 3;  // Movimiento aleatorio entre 0 y 3
+
+//encontrar una forma de poder modificar la cantidad de caballos y que funcione el while a sido sorprendentemente dificil
+    while (carreraEnCurso(listaCaballos) /*c1 < MAX_DISTANCE && c2 < MAX_DISTANCE && c3 < MAX_DISTANCE*/ )         {
+        /*c1 += rand() % 3;  // Movimiento aleatorio entre 0 y 3
         c2 += rand() % 3;
-        c3 += rand() % 3;
+        c3 += rand() % 3;*/
+        for (int i = 0; i < MAX_CABALLOS; i++)
+        {
+            listaCaballos[i].posicion += rand() % 3 ;
+            if (listaCaballos[i].posicion > MAX_DISTANCE) {
+                listaCaballos[i].posicion = MAX_DISTANCE;
+            }
+        }
 
         // Limpiar pantalla (Windows usa system("cls"))
         system("cls");
 
         // Imprimir la pista de la carrera
 
-        for (int i = 0; i < cantidadCaballos; i++)
+        for (int i = 0; i < MAX_CABALLOS; i++)
         {
-            imprimirPistaCarrera(i);
+            imprimirPistaCarrera(listaCaballos[i]);
         }
         
         /*printf("Caballo 1: |");
@@ -111,9 +127,9 @@ void startCarrera(Cliente *c){
     printf("\n¡Carrera terminada! Gracias por participar!\n");
     fflush(stdout);
 
-    for (int i = 0; i < cantidadCaballos; i++)
+    for (int i = 0; i < MAX_CABALLOS; i++)
     {
-        caballoGanador(i);
+        caballoGanador(listaCaballos[i]);
     }
 
     /*if (c1 >= MAX_DISTANCE){
@@ -130,20 +146,28 @@ void startCarrera(Cliente *c){
         printf("¡Caballo 3 gana!\n");
     	fflush(stdout);
     }*/
+    }
 
-}
-void imprimirPistaCarrera(int caballo){
-printf("Caballo %d: |", caballo+1);
+void imprimirPistaCarrera(Caballo caballo){
+printf("Caballo %d: |", caballo.numero);
         for (int i = 0; i <= MAX_DISTANCE; i++) {
-            if (i == caballo) printf("🐎");
+            if (i == caballo.posicion) printf("🐎");
             else if (i == MAX_DISTANCE) printf("| 🏁​ |"); // Meta al final
             else printf(" ");
         }
         printf("\n");
 }
-void caballoGanador(int caballo){
-    if (caballo >= MAX_DISTANCE){
-        printf("¡Caballo %d gana!\n", caballo+1);
+void caballoGanador(Caballo caballo){
+    if (caballo.posicion >= MAX_DISTANCE){
+        printf("¡Caballo %d gana!\n", caballo.numero);
     	fflush(stdout);
     }
+}
+int carreraEnCurso(Caballo listaCaballos[]) {//esta es la unica solucion que he encontrado al problema del while
+    for (int i = 0; i < MAX_CABALLOS; i++) {
+        if (listaCaballos[i].posicion >= MAX_DISTANCE) {
+            return 0;
+        }
+    }
+    return 1;
 }
--- carrera.h
+++ carrera.h
@@ -1,11 +1,16 @@
 #ifndef CARERRA_H_
 #define CARERRA_H_
 #include <stdio.h>
-
 #include "personas.h"
+
+
+typedef struct {
+    int numero;
+    int posicion;
+} Caballo;
 void carrera(Cliente c);
 void startCarrera(Cliente *c);
-void imprimirPistaCarrera(int caballo);
-void caballoGanador(int caballo);
-
+void imprimirPistaCarrera(Caballo caballo);
+void caballoGanador(Caballo caballo);
+int carreraEnCurso(Caballo listaCaballos[]);
 #endif // 
