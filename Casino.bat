5fb57c5f93ef00b2e646999c8cfd120e9e27215f
Gorka Villalba <145966880+GorkaVillalba@users.noreply.github.com>
2025-05-27 17:17:30 +0200

    odio los compiladores, he hecho una compilacion separada y para luego compilarlos juntos, por ahora es lo unico que funciona, en lo que queda de tarde terminare de modificar y arreglar el resto de clases

--- Casino.bat
+++ Casino.bat
@@ -1,5 +1,8 @@
 @echo off
 chcp 65001 >nul
 del DeustoCasino.exe
-gcc Menus/main.c Menus/menus.c Model/personas.c Persistencia/fichero.c Juegos/TragaPerras.c Juegos/dado.c Music/music.c Juegos/carrera.c Logger/logger.c Persistencia/config.c Juegos/blackjack.c Persistencia/bd.c Persistencia/sqlite3.c -o DeustoCasino -lwinmm -mconsole
+g++ -c Socket/socket.cpp -Iinclude -o Socket/socket.o
+gcc -c Menus/main.c Menus/menus.c Model/personas.c Persistencia/fichero.c Juegos/TragaPerras.c Juegos/dado.c Music/music.c Juegos/carrera.c Logger/logger.c Persistencia/config.c Juegos/blackjack.c Persistencia/bd.c Persistencia/sqlite3.c -Iinclude
+g++ *.o Socket/socket.o -o DeustoCasino -lwinmm -lws2_32 -mconsole
+del *.o
 DeustoCasino.exe
